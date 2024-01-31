0d826f0f8d05c92d7c20475340cbc1a5f6af5c94
Sunir Kishan Manandhar <137463109+sunirvelvet@users.noreply.github.com>
2024-01-31 20:51:38 +0000

    First

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import Showcase from "./Showcase";
+import image from "./images/bulbasaur.jpg"
+import './App.css';
+
+
 function App() {
-  return (
-    <div>
-      <h1>App Component</h1>
+  const favPokemon = "Bulbasaur";
+  const pokeCharacteristics = {type: "Grass", move:"Vine Whip"}
+  const styles = {
+    backgroundColor: 'green',
+    color: 'white'
+  }
+    return (
+    <div className='background'>
+      <Showcase/>
+      <h1>{favPokemon}'s Showcase Component</h1>
+      <img src={image} alt={favPokemon}
+      style={{width: '200px', height: '150px', borderRadius:'50%', objectFit: 'cover'}}/>
+      <h2>
+        <span style={styles}>
+          {pokeCharacteristics.type}
+        </span>
+        <span style = {styles}>
+          Bulbasaur's type is {pokeCharacteristics.type} and one of their moves is {pokeCharacteristics.move}.
+        </span>
+      </h2>
     </div>
   );
 }
---
+++ src/Showcase.js
@@ -0,0 +1,12 @@
+import './Showcase.css';
+
+
+function Showcase() {
+    return (
+      <div className="background center-container ">
+        <h1>Showcase Component</h1>
+      </div>
+    );
+  }
+  
+  export default Showcase;
